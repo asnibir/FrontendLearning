ef1a4975ad44332ccdafb197723373d289edcea1
Abu Shaikh Nibir <abushaikhnibir@gmial.com>
2023-03-22 08:57:25 +0600

    post...

--- Angular/FirstApp/src/app/posts/posts.component.ts
+++ Angular/FirstApp/src/app/posts/posts.component.ts
@@ -1,17 +1,48 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
+
 export class PostsComponent {
-  // posts: any[];
-  constructor(http: HttpClient){
-    http.get('https://jsonplaceholder.typicode.com/posts')
-        .subscribe(response => {
-          // this.posts = response.json();
-        });
+
+  posts: any[] = [];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: HttpClient) {
+    this.http.get<Post[]>(this.url)
+      .subscribe(response => {
+        this.posts = response; // httpClient automatically get json object
+        //console.log(response);
+      });
   }
+
+  createPost(input: HTMLInputElement) {
+    let post: any = { title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post.id = response;
+        this.posts.splice(0, 0, post);
+        console.log(response);
+      })
+  }
+
+  updatePost(post: any) {
+    //this.http.put(this.url, JSON.stringify(post));
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response);
+      })
+  }
+
 }
