a61f839384ee692975fd193e9184448d4db83a41
nibir <abu.shaikh.nibir@dohatec.com.bd>
2023-03-22 11:13:47 +0600

    Error Handling almost done...

--- Angular/FirstApp/src/app/posts/posts.component.ts
+++ Angular/FirstApp/src/app/posts/posts.component.ts
@@ -1,12 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-
-interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { NotFoundError } from 'rxjs';
+import { AppError } from '../app-error';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-posts',
@@ -14,35 +9,71 @@ interface Post {
   styleUrls: ['./posts.component.css']
 })
 
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+
 
   posts: any[] = [];
-  private url = 'https://jsonplaceholder.typicode.com/posts';
-  constructor(private http: HttpClient) {
-    this.http.get<Post[]>(this.url)
+
+  // Do not call http services in our constructor
+  constructor(private service: PostService) {
+
+  }
+  
+  ngOnInit(): void {
+    this.service.getPosts()
       .subscribe(response => {
         this.posts = response; // httpClient automatically get json object
         //console.log(response);
+      }, error => {
+        alert('An unexpected error occurred.');
+        console.log(error);
       });
   }
 
   createPost(input: HTMLInputElement) {
     let post: any = { title: input.value };
     input.value = '';
-    this.http.post(this.url, JSON.stringify(post))
+    this.service.createPost(post)
       .subscribe(response => {
         post.id = response;
         this.posts.splice(0, 0, post);
         console.log(response);
-      })
+      }, (error: Response) => {
+        if(error.status === 400)
+          alert('This post has already been created.');
+        else{
+          alert('An unexpected error occurred.');
+          console.log(error);
+        }
+      });
   }
 
   updatePost(post: any) {
     //this.http.put(this.url, JSON.stringify(post));
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+    this.service.updatePost(post)
       .subscribe(response => {
         console.log(response);
-      })
+      }, error => {
+        alert('An unexpected error occurred.');
+        console.log(error);
+      });
   }
 
+  // http delete request don't have a body
+  deletePost(post: any){
+    this.service.deletePost(post.id)
+      .subscribe(
+        response => {
+          let index = this.posts.indexOf(post);
+          this.posts.splice(index, 1); // delete 1 object
+        }, 
+        (error: AppError) => {
+          if(error instanceof NotFoundError)
+            alert('This post has already been deleted.');
+          else{
+            alert('An unexpected error occurred.');
+            console.log(error);
+          }
+        });
+  }
 }
